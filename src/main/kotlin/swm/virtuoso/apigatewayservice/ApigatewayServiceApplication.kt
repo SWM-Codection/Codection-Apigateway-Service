1fafcf724b35808a012f8bb7020a50ca782fb9b9
sangcheol12 <eric2057@daum.net>
2024-07-02 12:14:53 +0900

    my first commit

---
+++ src/main/kotlin/swm/virtuoso/apigatewayservice/ApigatewayServiceApplication.kt
@@ -0,0 +1,11 @@
+package swm.virtuoso.apigatewayservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApigatewayServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ApigatewayServiceApplication>(*args)
+}
